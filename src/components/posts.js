fc0ddb55ff6827733b673f0a65ac60a7224005c4
UltraMegaMango <gyuhankim@gmail.com>
2024-06-01 16:14:21 -0400

    fix the oopsie

--- src/components/posts.js
+++ src/components/posts.js
@@ -4,13 +4,13 @@ import { Body, FlexContainer } from "../styles/layout"
 
 const GifContainer = styled(FlexContainer)`
   margin-bottom: 24px;
-  border-radius: 32px;
 `
 
 const StyledGif = styled('img')`
   width: 100%;
   max-width: 640px;
   height: auto;
+  border-radius: 32px;
 `
 
 export const Posts = () => {
